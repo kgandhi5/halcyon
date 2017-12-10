919ced365b4ad95dd9f73645651a4e09db98cd22
kgandhi5 <heyitskushal@gmail.com>
2017-12-10 13:03:52 -0600

    add filterdata json

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, SelectMultipleField, RadioField
@@ -18,12 +18,24 @@ class FilterForm(FlaskForm):
 def index():
 	form = FilterForm()
 	if form.validate_on_submit():
+		
 		Danger.processInputs(form)
 	return render_template('index.html', form=form)
 
 @app.route('/map')
 def map():
 	return render_template('map.html')
+
+@app.route('/rest/get/filterdata', methods=('GET','POST'))
+def filterdata():
+	test_json = {
+			"name":{
+				"flower":"red",
+				"blue":3
+				}
+			}
+	test_json= {"geometry": {"type": "Point", "coordinates": [111.48074142925812, -5.2353587094093674]}, "type": "Feature", "properties": {}}
+	return jsonify(test_json)
 
 if __name__ == '__main__':
 	app.run(debug=True)
